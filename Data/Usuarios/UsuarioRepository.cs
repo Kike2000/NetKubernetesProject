ca7c99279a675247159df9fa2ea288186f1aea2b
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-11-19 18:54:07 -0600

    Actualización de UsuarioRepository

--- Data/Usuarios/UsuarioRepository.cs
+++ Data/Usuarios/UsuarioRepository.cs
@@ -1,22 +1,65 @@
+using Microsoft.AspNetCore.Identity;
 using NetKubernetes.Dtos.UsuarioDTO;
 using NetKubernetes.Models;
+using NetKubernetes.Token;
 
 namespace NetKubernetes.Data.Usuarios;
 
 public class UsuarioRepository : IUsuarioRepository
 {
-    public Task<UsuarioResponseDTO> GetUsuario()
+    private readonly UserManager<Usuario> _userManager;
+    private readonly SignInManager<Usuario> _signInManager;
+    private readonly IJwtGenerator _jwtGenerator;
+    private readonly ApplicationDbContext _context;
+    private readonly IUsuarioSesion _usuarioSesion;
+
+    public UsuarioRepository(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager,
+                            IJwtGenerator jwtGenerator, ApplicationDbContext context, IUsuarioSesion usuarioSesion)
     {
-        throw new NotImplementedException();
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _jwtGenerator = jwtGenerator;
+        _context = context;
+        _usuarioSesion = usuarioSesion;
+    }
+    private UsuarioResponseDTO TransformUserToUserDTO(Usuario usuario)
+    {
+        return new UsuarioResponseDTO
+        {
+            Id = usuario.Id,
+            Nombre = usuario.Nombre,
+            Apellido = usuario.Apellido,
+            Telefono = usuario.Telefono,
+            Email = usuario.Email,
+            UserName = usuario.UserName,
+            Token = _jwtGenerator.CrearToken(usuario)
+        };
     }
 
-    public Task<UsuarioResponseDTO> Login(UsuarioLoginRequestDTO request)
+    public async Task<UsuarioResponseDTO> GetUsuario()
     {
-        throw new NotImplementedException();
+        var usuario = await _userManager.FindByNameAsync(_usuarioSesion.ObtenerUsuarioSesion());
+        return TransformUserToUserDTO(usuario!);
     }
 
-    public Task<UsuarioResponseDTO> RegistroUsuario(UsuarioRegistroRequestDTO request)
+    public async Task<UsuarioResponseDTO> Login(UsuarioLoginRequestDTO request)
     {
-        throw new NotImplementedException();
+        var usuario = await _userManager.FindByEmailAsync(request.Email!);
+        await _signInManager.CheckPasswordSignInAsync(usuario!, request.Password!, false);
+        return TransformUserToUserDTO(usuario!);
+    }
+
+    public async Task<UsuarioResponseDTO> RegistroUsuario(UsuarioRegistroRequestDTO request)
+    {
+        var usuario = new Usuario
+        {
+            Nombre = request.Nombre,
+            Apellido = request.Apellido,
+            Telefono = request.Telefono,
+            Email = request.Email,
+            UserName = request.UserName
+        };
+        await _userManager.CreateAsync(usuario!, request.Password!);
+        return TransformUserToUserDTO(usuario);
     }
 }
