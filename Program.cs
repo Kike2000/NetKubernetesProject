4a83eaefb67431e55292663746adc8547ffa358f
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-11-20 19:20:16 -0600

    Db agregada mediante migraciones

--- Program.cs
+++ Program.cs
@@ -1,11 +1,82 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NetKubernetes.Data;
+using NetKubernetes.Data.Inmuebles;
+using NetKubernetes.Data.Usuarios;
+using NetKubernetes.Middleware;
+using NetKubernetes.Models;
+using NetKubernetes.Profiles;
+using NetKubernetes.Token;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.LogTo(Console.WriteLine, new[] {
+        DbLoggerCategory.Database.Command.Name },
+        LogLevel.Information).EnableSensitiveDataLogging();
+
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")!);
+});
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+builder.Services.AddControllers(opt =>
+{
+    //Investigar
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Investigar
+var mapperConfig = new MapperConfiguration(m =>
+{
+    m.AddProfile(new InmuebleProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+
+
+//Investigar
+var builderSecurity = builder.Services.AddIdentityCore<Usuario>();
+var identityBuilder = new IdentityBuilder(builderSecurity.UserType, builder.Services);
+identityBuilder.AddEntityFrameworkStores<ApplicationDbContext>();
+identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+
+builder.Services.AddSingleton<ISystemClock, SystemClock>();
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+builder.Services.AddScoped<IUsuarioSesion, UsuarioSesion>();
+builder.Services.AddScoped<IInmuebleRepository, InmuebleRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddSingleton(mapper);
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
+
+builder.Services.AddCors(o => o.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -16,11 +87,30 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ManagerMiddleware>();
+app.UseCors("corsapp");
 app.UseAuthentication();
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var env = app.Services.CreateScope())
+{
+    var services = env.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<Usuario>>();
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        await context.Database.MigrateAsync();
+        await LoadDatabase.InsertarData(context, userManager);
+    }
+    catch (Exception ex)
+    {
+        var logging = services.GetRequiredService<ILogger<Program>>();
+        logging.LogError(ex, "Error en la migración");
+    }
+}
 
 app.Run();
